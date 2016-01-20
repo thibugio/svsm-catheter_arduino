10f3dd4bbb9bbdc961d26f1834d880ee0bb4927c
thibugio <rmf61@case.edu>
2016-01-19 23:33:23 -0500

    last commit before wiping windows

--- catheter_arduino/__vm/.catheter_arduino.vsarduino.h
+++ catheter_arduino/__vm/.catheter_arduino.vsarduino.h
@@ -13,74 +13,78 @@
 #define _VMDEBUG 1
 #define printf iprintf
 #define F_CPU 84000000L
-#define ARDUINO 10607
+#define ARDUINO 10708
 #define ARDUINO_SAM_DUE
 #define ARDUINO_ARCH_SAM
 #define __SAM3X8E__
-#define USB_VID 0x2341
+#define USB_VID 0x2a03
 #define USB_PID 0x003e
 #define USBCON
 #define __cplusplus
-#define __ARM__
 #define __inline__
 #define __asm__(x)
 #define __extension__
 #define __ATTR_PURE__
 #define __ATTR_CONST__
 #define __inline__
+#define __asm__ 
 #define __volatile__
-#define __SIZE_TYPE__ long
 
 #define __ICCARM__
 #define __ASM
 #define __INLINE
-#define __builtin_va_list void
-//#define _GNU_SOURCE 
 //#define __GNUC__ 0
-//#undef  __ICCARM__
-//#define __GNU__
-
-typedef long Pio;
-typedef long Efc;
-typedef long Adc;
-typedef long Pwm;
-typedef long Rtc;
-typedef long Rtt;
-typedef long pRtc;
-typedef long Spi;
-typedef long spi;
-typedef long Ssc;
-//typedef long p_scc;
-typedef long Tc;
-//typedef long pTc;
-typedef long Twi;
-typedef long Wdt;
-//typedef long pTwi;
-typedef long Usart;
-typedef long Pdc;
-typedef long Rstc;
-
-extern const int ADC_MR_TRGEN_DIS = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
-extern const int ADC_MR_TRGSEL_Pos = 0;
-
-extern const int ADC_MR_TRGSEL_Msk = 0;
-extern const int ADC_MR_TRGEN = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
+#define __GNUC__ 4
+#define __GNUC_MINOR__ 100
 
 
-
+#define __ICCARM__
 #define __ARMCC_VERSION 400678
 #define __attribute__(noinline)
 
 #define prog_void
 #define PGM_VOID_P int
+            
 
+extern "C" void __cxa_pure_virtual() {;}
 
 typedef unsigned char byte;
-extern "C" void __cxa_pure_virtual() {;}
+typedef int __builtin_va_list;
+
+//arduino.org
+typedef long Pwm;
+typedef long Adc;
+typedef long Efc;
+typedef long Pio;
+typedef long Rstc;
+typedef long Rtc;
+typedef long Rtt;
+typedef long Spi;
+typedef long Ssc;
+typedef long Tc;
+typedef long Twi;
+typedef long Usart;
+typedef long Wdf;
+typedef long Wdt;
+typedef long Pdc;
+typedef long caddr_t;
+
+
+#define ADC_MR_TRGSEL_Pos 0
+#define ADC_MR_TRGSEL_Msk 0
+
+#define ADC_MR_TRGSEL_ADC_G0 0
+#define ADC_MR_TRGSEL_ADC_G1 0
+#define ADC_MR_TRGSEL_ADC_G2 0
+#define ADC_MR_TRGSEL_ADC_G3 0
+
+#define ADC_MR_TRGEN 0
+#define ADC_MR_TRGEN_DIS 0
+#define ADC_MR_TRGSEL_ADC_TRIG0 0
+#define ADC_MR_TRGSEL_ADC_TRIG1 0
+#define ADC_MR_TRGSEL_ADC_TRIG2 0
+#define ADC_MR_TRGSEL_ADC_TRIG3 0
+
 
 
 
